cc4a7a9a35090d698272f2fe5323cbea696183bd
imjuliayue <juliayue6@gmail.com>
2025-07-30 15:09:51 -0400

    Finished creating word clouds

--- wordVisualize.py
+++ wordVisualize.py
@@ -3,23 +3,30 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 # METHODS
-# def plot_wordcloud()
+def plot_wordcloud(text, title):
+    wordCloud = WordCloud(width = 800, height = 400, background_color= "white", max_words = 200).generate(text)
+    plt.figure(figsize=(15, 7.5))
+    plt.imshow(wordCloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.title(title)
+    plt.savefig(f"graphics/{title.replace(" ", "_")}.png")
+    plt.show()
 
 # LOAD THE CLEANED DATASETS
 Fakedf = pd.read_csv('data/Fake_cleaned.csv')
 Truedf = pd.read_csv('data/True_cleaned.csv')
 
-print(Fakedf["text"][0])
+print(Fakedf["text"][1])
 
 # # CREATE A WORDCLOUD
-# Fakedf['full_text'] = Fakedf['title'].fillna('') + ' ' + Fakedf['text'].fillna('')
-# Truedf['full_text'] = Truedf['title'].fillna('') + ' ' + Truedf['text'].fillna('')
+Fakedf['full_text'] = Fakedf['title'].fillna('') + ' ' + Fakedf['text'].fillna('')
+Truedf['full_text'] = Truedf['title'].fillna('') + ' ' + Truedf['text'].fillna('')
 
-# print(Fakedf['full_text'][0])
+FakeText = " ".join(Fakedf['full_text'].astype(str).tolist())
+TrueText = " ".join(Truedf['full_text'].astype(str).tolist())
 
-# FakeText = " ".join(Fakedf['full_text'].astype(str).tolist())
-# print(FakeText[0])
-
+plot_wordcloud(FakeText, "Fake News Word Cloud")
+plot_wordcloud(TrueText, "True News Word Cloud")
 
 
 # PLOT MOST FREQUENT WORDS
