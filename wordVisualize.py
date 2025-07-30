2c4ed9cc651895b0156a024af5368959c381c22f
imjuliayue <juliayue6@gmail.com>
2025-07-30 16:49:25 -0400

    Finished text length distributions, ERROR: note that might need to normalize the axis

--- wordVisualize.py
+++ wordVisualize.py
@@ -12,18 +12,36 @@ def plot_wordcloud(text, title):
     plt.savefig(f"graphics/{title.replace(" ", "_")}.png")
     plt.show()
 
-def plot_wordfreq(wordFreq, title):
+def plot_wordfreq(wordFreq, title, xlabel):
     words, counts = zip(*wordFreq)
 
     plt.figure(figsize=(12, 6))
     plt.bar(words, counts)
     plt.xticks(rotation=45)
     plt.title(title)
-    plt.xlabel('Words')
+    plt.xlabel(xlabel)
     plt.ylabel('Frequency')
     plt.tight_layout()
     plt.savefig(f"graphics/{title.replace(' ', '_')}.png")
     plt.show()
+
+def bucketize(wordLens, bucketSize=2000):
+    from collections import defaultdict
+    import numpy as np
+    values = [x[0] for x in wordLens]
+    frequencies = [x[1] for x in wordLens]
+    bin_edges = np.arange(0, max(values) + bucketSize, bucketSize)
+    bin_indices = np.digitize(values, bins=bin_edges, right=False)
+    buckets = defaultdict(int)
+
+    for idx, freq in zip(bin_indices,frequencies):
+        buckets[idx]+=freq
+
+    bucket_labels=[f"{bin_edges[i]}-{bin_edges[i+1]}" for i in range(len(bin_edges)-1)]
+    bucket_counts = [buckets[i] for i in sorted(buckets.keys())]
+
+    return bucket_labels, bucket_counts
+
 
 # LOAD THE CLEANED DATASETS
 Fakedf = pd.read_csv('data/Fake_cleaned.csv')
@@ -48,8 +66,8 @@ FakeCommonWords = wordFreq.most_common(20)
 wordFreq = Counter(TrueText.split())
 TrueCommonWords = wordFreq.most_common(20)
 
-# plot_wordfreq(FakeCommonWords, "Fake Common Words (Top 20)")
-# plot_wordfreq(TrueCommonWords, "True Common Words (Top 20)")
+# plot_wordfreq(FakeCommonWords, "Fake Common Words (Top 20)", "Words")
+# plot_wordfreq(TrueCommonWords, "True Common Words (Top 20)", "Words")
 
 # PLOT MOST FREQUENT NGRAMS
 
@@ -59,7 +77,7 @@ FakeLens = Fakedf['title'].str.split()
 FakeLens = list(map(len, FakeLens))
 wordFreq = Counter(FakeLens)
 FakeLens = sorted(wordFreq.items())
-plot_wordfreq(FakeLens, "Fake Title Length Distribution")
+# plot_wordfreq(FakeLens, "Fake Title Length Distribution", "Title Length")
 
 TrueLens = Truedf['title'].str.split()
 TrueLens = list(map(len, TrueLens))
@@ -67,11 +85,27 @@ wordFreq = Counter(TrueLens)
 for i in range(16,31):
     wordFreq[i] = 0
 TrueLens = sorted(wordFreq.items())
-plot_wordfreq(TrueLens, "True Title Length Distribution")
+# plot_wordfreq(TrueLens, "True Title Length Distribution", "Title Length")
 
-print(FakeLens)
-print(TrueLens)
 
 # PLOT WORD LENGTH DISTRIBUTION
 
 # PLOT TEXT LENGTH DISTRIBUTION
+FakeLens = Fakedf['text'].str.split()
+FakeLens = list(map(len,map(str, FakeLens)))
+wordFreq = Counter(FakeLens)
+FakeLens = sorted(wordFreq.items())
+
+TrueLens = Truedf['text'].str.split()
+TrueLens = list(map(len,map(str, TrueLens)))
+wordFreq = Counter(TrueLens)
+TrueLens = sorted(wordFreq.items())
+
+# Create buckets
+bucketSize = 2000
+
+Fakelabels, FakeCounts = bucketize(FakeLens, bucketSize)
+Truelabels, TrueCounts = bucketize(TrueLens, bucketSize)
+
+plot_wordfreq(zip(Fakelabels, FakeCounts), "Fake Text Length Distribution", "Text Length")
+plot_wordfreq(zip(Truelabels, TrueCounts), "Real Text Length Distribution", "Text Length")
