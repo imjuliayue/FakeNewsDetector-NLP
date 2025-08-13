2d085ba63c80c493bf6e0c20976a2fa3c9afef14
imjuliayue <juliayue6@gmail.com>
2025-08-13 13:59:15 -0400

    Beginning to work on sentiment analysis. Preparing to reclean data for vader and sentiment analysis

--- sklearn models/SentimentModels.py
+++ sklearn models/SentimentModels.py
@@ -1,8 +1,14 @@
 # IMPORTS
 import pandas
 import spacy
+import nltk
 
 from functionsLR import *
 
 # pre-trained English language processing pipeline by spaCy (contains 300d vectors for all words)
 nlp = spacy.load('en_core_web_md')
+
+# nltk model that maps lexical features to strength of sentiments (words/features => +/- score where amplitude = strength)
+nltk.download('vader_lexicon')
+
+
