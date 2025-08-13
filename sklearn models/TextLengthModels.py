da6187b65dd4070bd1ae8edaaf24344099373bcb
imjuliayue <juliayue6@gmail.com>
2025-08-13 17:03:42 -0400

    Finished setting up TextMetadata model for logistic regression

--- sklearn models/TextLengthModels.py
+++ sklearn models/TextLengthModels.py
@@ -7,10 +7,16 @@ ytrain = loadData("data/Vader", "y_train")
 Xtest = loadDataUTF8("data/Vader", "X_test")
 ytest = loadData("data/Vader", "y_test")
 
+# Pipelines expect numeric values
+ytrain = [int(y) for y in ytrain]
+ytest = [int(y) for y in ytest]
+
 # BEGIN TEXT LENGTH MODEL TRAINING --------------------------
 
 # Logistic regression
 from sklearn.linear_model import LogisticRegression
 
 pipeline = [("TextMetadata", TextMetadataTransformer()),("LogReg",LogisticRegression())]
+
+print(pipelineAllMetrics(Xtrain,ytrain,Xtest,ytest,pipeline,"LogisticRegression/TextMetadata",n_splits=3))
 
