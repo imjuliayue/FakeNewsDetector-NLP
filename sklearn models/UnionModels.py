7e2074ae5d25f0b9f5dfde34532c86168044d89a
imjuliayue <juliayue6@gmail.com>
2025-08-13 23:13:53 -0400

    Wrote Union model, but there is a problem with the deep copying because it cannot do it recursively

---
+++ sklearn models/UnionModels.py
@@ -0,0 +1,29 @@
+from functionsLR import *
+
+# Load in the data
+print("loading data...")
+Xtrain = loadDataUTF8("data/Vader", "X_train")
+ytrain = loadData("data/Vader", "y_train")
+Xtest = loadDataUTF8("data/Vader", "X_test")
+ytest = loadData("data/Vader", "y_test")
+
+# Pipelines expect numeric values
+ytrain = [int(y) for y in ytrain]
+ytest = [int(y) for y in ytest]
+
+from sklearn.pipeline import FeatureUnion,Pipeline
+from sklearn.linear_model import LogisticRegression
+
+# CONSTRUCT THE UNION OF THE TWO BADLY-PERFORMING FEATURES
+sentimentPipeline = [("VADERSentiments", VADERTransformer())]
+
+textMetadataPipeline = [("Metadata", TextMetadataTransformer)]
+
+sentimentMetaUnion = FeatureUnion([("Sentiment",sentimentPipeline),("Metadata",textMetadataPipeline)])
+
+
+# Logistic Regression
+
+pipeline = Pipeline([("sentMetaUnion",sentimentMetaUnion),("LogReg",LogisticRegression)])
+
+print(pipelineAllMetrics(Xtrain,ytrain,Xtest,ytest,pipeline,"LogisticRegression/Union",n_splits=3))
